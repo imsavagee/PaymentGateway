78b634f6e339bc28bbe75b2005a24575fbc19425
shavezjeetart <shavez@jeetart.com>
2024-10-09 10:12:45 +0530

    modify:vite & package

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/PaymentGateway/",
   plugins: [react()],
-  base:"/PaymentGateway/"
+  
 })
